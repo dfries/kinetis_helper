cfec157a52a4da7f0fb792ddf67f3e5e29b6f62a
David Fries <David@Fries.net>
2015-11-20 03:38:15 -0600

    define other useful macros and such here

---
+++ bit_extra.h
@@ -0,0 +1,11 @@
+/* Copyright (c) 2015 David Fries <David@Fries.net>  all rights reserved
+ */
+
+#ifndef __bit_extra_h__
+#define __bit_extra_h__
+
+#ifndef _BV
+#define _BV(n)          (1<<(n))
+#endif
+
+#endif // __bit_extra_h__
